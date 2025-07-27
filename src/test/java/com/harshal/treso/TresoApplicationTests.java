7123ade95d2560a8f3970cac98baf2b63cb0c25d
Harshal Patel <harshal4897@gmail.com>
2025-07-27 00:08:19 -0500

    Initial commit: Treso project setup

---
+++ src/test/java/com/harshal/treso/TresoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshal.treso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
