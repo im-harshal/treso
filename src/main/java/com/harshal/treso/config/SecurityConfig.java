1904d480588cef4eaca3a81ac854db7be700c6a9
Harshal Patel <harshal4897@gmail.com>
2025-08-03 00:19:20 -0500

    Added in Actuator and Swagger UI changes

--- src/main/java/com/harshal/treso/config/SecurityConfig.java
+++ src/main/java/com/harshal/treso/config/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
         http.csrf().disable()
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/actuator/health", "/actuator/info", "/actuator/metrics").permitAll()
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs.yaml","/docs").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
